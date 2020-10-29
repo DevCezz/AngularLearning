df0c1f7001c287e5c390cbb50a93956dd83e3437
Cezary Sanecki <devcezz@gmail.com>
2020-10-29 23:44:39 +0100

    Add example of property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1>{{ name.toUpperCase() }}</h1>
+    <h1 [style.color]="nameColor" [class.isActive]="isActive">{{ name.toUpperCase() }}</h1>
+    <img [src]="photoUrl" />
   `,
-  styles: []
+  styles: [
+    '.isActive { text-decoration: underline; }'
+  ]
 })
 export class AppComponent {
   name: string = "Jan Kowalski";
+  nameColor: string = "blue";
+  isActive: boolean = true;
+  photoUrl: string = "assets/images/customer.png";
 
   constructor() {}
 }
