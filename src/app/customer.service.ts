f836c1163e486d3d9d68d991e050fbab674d8f04
Cezary Sanecki <devcezz@gmail.com>
2020-10-30 20:08:37 +0100

    Inject HttpClient in CustomerService

--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { CONFIG, Config, Customer, CustomerType } from './model';
 
@@ -59,7 +60,10 @@ export class CustomerService {
     }
   ];
 
-  constructor(@Inject(CONFIG) private config: Config) { }
+  constructor(
+    @Inject(CONFIG) private config: Config,
+    private httpClient: HttpClient
+  ) { }
 
   getCustomers() {
     return this.customers.slice(0, this.config.customerLimit);
