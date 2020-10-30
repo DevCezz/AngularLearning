aaa76b1e21b9d50589e037a2e2668fab61890fbe
Cezary Sanecki <devcezz@gmail.com>
2020-10-30 19:20:34 +0100

    Define Config object with limit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { HighlightDirective } from './highlight.directive';
 import { CustomerBrowserComponent } from './customer-browser/customer-browser.component';
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 import { CustomerService } from './customer.service';
+import { Config } from './model';
+
+const config: Config = {
+  customerLimit: 10
+}
 
 @NgModule({
   declarations: [
