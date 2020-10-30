2edb71a7f29247fc48819bdffdc5d71049845473
Cezary Sanecki <devcezz@gmail.com>
2020-10-30 12:35:22 +0100

    Add customers to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,23 +52,58 @@ import { Customer, CustomerType } from './model';
   ]
 })
 export class AppComponent {
-  customer: Customer = {
-    name: "Jan Kowalski",
-    photoUrl: "assets/images/customer.png",
-    age: 43,
-    description: "Very important client",
-    address: {
-      street: "Zielona",
-      houseNumber: 5,
-      city: "Warszawa"
+  customers: Customer[] = [
+    {
+      name: "Jan Kowalski",
+      photoUrl: "assets/images/customer.png",
+      age: 43,
+      description: "Very important client",
+      address: {
+        street: "Zielona",
+        houseNumber: 5,
+        city: "Warszawa"
+      },
+      type: CustomerType.Premium,
+      categories: [
+        "zagraniczny",
+        "mikroprzedsiębiorstwo",
+        "duży obrót"
+      ]
     },
-    type: CustomerType.Premium,
-    categories: [
-      "zagraniczny",
-      "mikroprzedsiębiorstwo",
-      "duży obrót"
-    ]
-  };
+    {
+      name: "Maria Stankiewicz",
+      photoUrl: "assets/images/customer.png",
+      age: 31,
+      description: "Important client",
+      address: {
+        street: "Dolna",
+        houseNumber: 2,
+        city: "Kraków"
+      },
+      type: CustomerType.Standard,
+      categories: [
+        "osoba prywatna"
+      ]
+    },
+    {
+      name: "Tadeusz Morawiecki",
+      photoUrl: "assets/images/customer.png",
+      age: 71,
+      description: "Long-term client",
+      address: {
+        street: "Urbańska",
+        houseNumber: 10,
+        city: "Łódź"
+      },
+      type: CustomerType.Premium,
+      categories: [
+        "emeryt",
+        "tylko gotówka",
+        "branża krawiecka"
+      ]
+    }
+  ];
+  customer: Customer = this.customers[0];
 
   nameColor: string = "blue";
   isActive: boolean = true;
