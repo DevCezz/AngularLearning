9e0ab5af3e01d00e8ba6af58a68de45f214bdf8e
Cezary Sanecki <devcezz@gmail.com>
2020-10-30 13:27:57 +0100

    Move code of CustomerBrowser to proper ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,66 +11,12 @@ import { Customer, CustomerType } from './model';
   ]
 })
 export class AppComponent {
-  customers: Customer[] = [
-    {
-      name: "Jan Kowalski",
-      photoUrl: "assets/images/customer.png",
-      age: 43,
-      description: "Very important client",
-      address: {
-        street: "Zielona",
-        houseNumber: 5,
-        city: "Warszawa"
-      },
-      type: CustomerType.Premium,
-      categories: [
-        "zagraniczny",
-        "mikroprzedsiębiorstwo",
-        "duży obrót",
-        "branża malarska"
-      ]
-    },
-    {
-      name: "Maria Stankiewicz",
-      photoUrl: "assets/images/customer.png",
-      age: 31,
-      description: "Important client",
-      address: {
-        street: "Dolna",
-        houseNumber: 2,
-        city: "Kraków"
-      },
-      type: CustomerType.Standard,
-      categories: [
-        "osoba prywatna"
-      ]
-    },
-    {
-      name: "Tadeusz Morawiecki",
-      photoUrl: "assets/images/customer.png",
-      age: 71,
-      description: "Long-term client",
-      address: {
-        street: "Urbańska",
-        houseNumber: 10,
-        city: "Łódź"
-      },
-      type: CustomerType.Premium,
-      categories: [
-        "emeryt",
-        "tylko gotówka",
-        "branża krawiecka"
-      ]
-    }
-  ];
+
   customer: Customer = null;
 
   nameColor: string = "blue";
   isActive: boolean = true;
   showPhoto: boolean = false;
-  // utworzenie pola o takiej samej nazwie jak enum daje możliwość korzystania,
-  // ze stałych switcha a nie z wartości liczbowych
-  CustomerType = CustomerType;
 
   constructor() {}
 
