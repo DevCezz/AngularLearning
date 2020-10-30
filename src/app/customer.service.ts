27a620543f57d202d0e848ccfffc3d3f14c8bbbe
Cezary Sanecki <devcezz@gmail.com>
2020-10-30 20:09:23 +0100

    Delete in memory data of customers

--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -7,59 +7,6 @@ import { CONFIG, Config, Customer, CustomerType } from './model';
 })
 export class CustomerService {
   
-  private customers: Customer[] = [
-    {
-      name: "Jan Kowalski",
-      photoUrl: "assets/images/customer.png",
-      age: 43,
-      description: "Very important client",
-      address: {
-        street: "Zielona",
-        houseNumber: 5,
-        city: "Warszawa"
-      },
-      type: CustomerType.Premium,
-      categories: [
-        "zagraniczny",
-        "mikroprzedsiębiorstwo",
-        "duży obrót",
-        "branża malarska"
-      ]
-    },
-    {
-      name: "Maria Stankiewicz",
-      photoUrl: "assets/images/customer.png",
-      age: 31,
-      description: "Important client",
-      address: {
-        street: "Dolna",
-        houseNumber: 2,
-        city: "Kraków"
-      },
-      type: CustomerType.Standard,
-      categories: [
-        "osoba prywatna"
-      ]
-    },
-    {
-      name: "Tadeusz Morawiecki",
-      photoUrl: "assets/images/customer.png",
-      age: 71,
-      description: "Long-term client",
-      address: {
-        street: "Urbańska",
-        houseNumber: 10,
-        city: "Łódź"
-      },
-      type: CustomerType.Premium,
-      categories: [
-        "emeryt",
-        "tylko gotówka",
-        "branża krawiecka"
-      ]
-    }
-  ];
-
   constructor(
     @Inject(CONFIG) private config: Config,
     private httpClient: HttpClient
