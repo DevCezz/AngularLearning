d410337b7fd24e833e94473011afcab6ddb4d57b
Cezary Sanecki <devcezz@gmail.com>
2020-10-30 18:51:08 +0100

    Create example with DI

---
+++ sandbox/dependency-injection.ts
@@ -0,0 +1,16 @@
+abstract class Engine {}
+
+class DieselEngine extends Engine {}
+
+class PetrolEngine extends Engine {}
+
+abstract class Gearbox {}
+
+class AutomaticGearbox extends Gearbox {}
+
+class ManualGearbox extends Gearbox {}
+
+class Car {
+    gearbox: Gearbox;
+    engine: Engine;
+}
