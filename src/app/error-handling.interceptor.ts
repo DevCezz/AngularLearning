0ee842807a6e5feabc2a5d3e2b0b739a1ce2f1b4
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 13:32:05 +0100

    Annotate ErrorHandling with Injectable to inject services

--- src/app/error-handling.interceptor.ts
+++ src/app/error-handling.interceptor.ts
@@ -1,7 +1,9 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MessageService } from './message.service';
 
+@Injectable()
 export class ErrorHandlingInterceptor implements HttpInterceptor {
 
     constructor(private messageService: MessageService) { }
