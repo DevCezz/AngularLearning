349ec094ff7a9c663cc8d553cde0742fd08b073f
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:55:05 +0100

    Remove ContractsModule to make use of lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
-import { ContractsModule } from './contracts/contracts.module';
 import { CustomersModule } from './customers/customers.module';
 import { CoreModule } from './core/core.module';
 import { RouterModule, Routes } from '@angular/router';
@@ -28,7 +27,6 @@ const routes: Routes = [
     HttpClientModule,
     RouterModule.forRoot(routes),
     ToastrModule.forRoot(),
-    ContractsModule,
     CustomersModule
   ],
   bootstrap: [AppComponent]
