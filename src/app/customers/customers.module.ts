d98a20f3964b80f045c46556966af25ccf59d880
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 16:25:21 +0100

    Tidy up Customers Module

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { CustomerAddComponent } from './customer-add/customer-add.component';
+import { CustomerBrowserComponent } from './customer-browser/customer-browser.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomerService } from './customer.service';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CustomerAddComponent,
+    CustomerBrowserComponent,
+    CustomerDetailsComponent
+  ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    CustomerService
+  ],
+  exports: [
+    CustomerAddComponent,
+    CustomerBrowserComponent
   ]
 })
 export class CustomersModule { }
