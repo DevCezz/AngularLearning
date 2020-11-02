88a73392f5657672502c9ac9d9d000422df92d04
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 13:47:51 +0100

    Handle HttpErrors in interceptor

--- src/app/error-handling.interceptor.ts
+++ src/app/error-handling.interceptor.ts
@@ -1,7 +1,10 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MessageService } from './message.service';
+
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class ErrorHandlingInterceptor implements HttpInterceptor {
@@ -10,6 +13,13 @@ export class ErrorHandlingInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log(req);
-        return next.handle(req);
+        return next.handle(req)
+            .pipe(catchError(error => {
+                if (error instanceof HttpErrorResponse) {
+                    this.messageService.error(`Błąd połączenia: ${error.message}`);
+                }
+
+                return throwError(error);
+            }));
     }
 }
