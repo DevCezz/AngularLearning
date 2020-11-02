01888095e9fddd5b4b440fbc02cec4627d538289
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:32:48 +0100

    Implement interface of deactivating add form

--- src/app/customers/customer-add-deactivate-guard.service.ts
+++ src/app/customers/customer-add-deactivate-guard.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CustomerAddComponent } from './customer-add/customer-add.component';
 
 @Injectable()
-export class CustomerAddDeactivateGuard {
+export class CustomerAddDeactivateGuard implements CanDeactivate<CustomerAddComponent> {
 
   constructor() { }
+  
+  canDeactivate(component: CustomerAddComponent, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    throw new Error('Method not implemented.');
+  }
 }
