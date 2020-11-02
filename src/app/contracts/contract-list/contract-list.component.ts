057058e8d44a904c3cfcefe6e707a6dea3a07317
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 15:52:57 +0100

    Add field of contracts

--- src/app/contracts/contract-list/contract-list.component.ts
+++ src/app/contracts/contract-list/contract-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contract } from '../model';
 
 @Component({
   selector: 'app-contract-list',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class ContractListComponent implements OnInit {
+
+  constracts: Contract[];
 
   constructor() { }
 
