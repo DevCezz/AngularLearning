08c267ae7724a797246f975e0dbce9cd1324a508
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 15:41:25 +0100

    Add component of contracts list

---
+++ src/app/contracts/contract-list/contract-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contract-list',
+  templateUrl: './contract-list.component.html',
+  styles: [
+  ]
+})
+export class ContractListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/contracts/contracts.module.ts
+++ src/app/contracts/contracts.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContractListComponent } from './contract-list/contract-list.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ContractListComponent],
   imports: [
     CommonModule
   ]
