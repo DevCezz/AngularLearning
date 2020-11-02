81ffb6e793cd6c44cb6bdaa7ceffda0b1316b907
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:12:29 +0100

    Provide AuthGuard

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { ErrorHandlingInterceptor } from './error-handling.interceptor';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuard } from './auth-guard.service';
 
 const config: Config = {
   customerLimit: 10,
@@ -19,7 +20,8 @@ const config: Config = {
   providers: [
     MessageService,
     { provide: CONFIG, useValue: config },
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorHandlingInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorHandlingInterceptor, multi: true },
+    AuthGuard
   ],
   declarations: [NavbarComponent, NotFoundComponent],
   exports: [
