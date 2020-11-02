988924a48fe0c4f60378874d9d795eadaa04ab94
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:31:25 +0100

    Change name of guard

--- src/app/customers/customer-add-deactivate-guard.service.ts
+++ src/app/customers/customer-add-deactivate-guard.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class CustomerAddDeactivateGuardService {
+@Injectable()
+export class CustomerAddDeactivateGuard {
 
   constructor() { }
 }
