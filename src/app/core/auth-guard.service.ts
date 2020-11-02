9ac94d3866d8a2a4b71c91b4872a5fa261c1df74
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:24:12 +0100

    Add condition to check if user is logged

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -12,6 +12,6 @@ export class AuthGuard implements CanActivate {
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
     console.log(state.url);
-    return true;
+    return this.authService.isLogged;
   }
 }
