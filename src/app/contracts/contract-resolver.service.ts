16ef3ed035b2f6eb556de06c50360f52a3986a7f
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:44:09 +0100

    Inject ContractService into resolver

--- src/app/contracts/contract-resolver.service.ts
+++ src/app/contracts/contract-resolver.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ContractService } from './contract.service';
 import { Contract } from './model';
 
 @Injectable()
 export class ContractResolver implements Resolve<Contract> {
 
-  constructor() { }
+  constructor(
+    private contractService: ContractService
+  ) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Contract | Observable<Contract> | Promise<Contract> {
     throw new Error('Method not implemented.');
