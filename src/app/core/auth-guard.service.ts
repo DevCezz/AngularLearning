dbb26b8e78e312a1f0e5d4021b36779e7ff9d2af
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:09:19 +0100

    Change naming to guard

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuardService {
+@Injectable()
+export class AuthGuard {
 
   constructor() { }
 }
