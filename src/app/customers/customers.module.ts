a989aad34e9d9c6e8e3b939ab5aa709841b79c1d
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 17:31:13 +0100

    Clean customer module to use shared module

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,24 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { CustomerAddComponent } from './customer-add/customer-add.component';
 import { CustomerBrowserComponent } from './customer-browser/customer-browser.component';
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 import { CustomerService } from './customer.service';
-import { FormsModule } from '@angular/forms';
-import { HighlightDirective } from '../highlight.directive';
-import { CapitalizePipe } from '../capitalize.pipe';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     CustomerAddComponent,
     CustomerBrowserComponent,
     CustomerDetailsComponent,
-    HighlightDirective,
-    CapitalizePipe
   ],
   imports: [
-    CommonModule,
-    FormsModule,
+    SharedModule
   ],
   providers: [
     CustomerService
