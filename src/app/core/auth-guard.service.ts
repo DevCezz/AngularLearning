2273530ac7df6cdb6dbe9f927391f54be6b6558c
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:11:39 +0100

    Change to basic implementation

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -8,6 +8,7 @@ export class AuthGuard implements CanActivate {
   constructor() { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-    throw new Error('Method not implemented.');
+    console.log(state.url);
+    return true;
   }
 }
