1da630469b687eff1c2f6beab219e049e24b21aa
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 16:05:03 +0100

    Add ContractService as provider

--- src/app/contracts/contracts.module.ts
+++ src/app/contracts/contracts.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContractListComponent } from './contract-list/contract-list.component';
+import { ContractService } from './contract.service';
 
 @NgModule({
   declarations: [ContractListComponent],
   imports: [
     CommonModule
+  ],
+  providers: [
+    ContractService
   ]
 })
 export class ContractsModule { }
