bc99b86d71b929a2429a0ceee8070f388bd9553f
Cezary Sanecki <devcezz@gmail.com>
2020-11-02 23:11:08 +0100

    Implement CanActivate interface

--- src/app/core/auth-guard.service.ts
+++ src/app/core/auth-guard.service.ts
@@ -1,7 +1,13 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class AuthGuard {
+export class AuthGuard implements CanActivate {
 
   constructor() { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    throw new Error('Method not implemented.');
+  }
 }
